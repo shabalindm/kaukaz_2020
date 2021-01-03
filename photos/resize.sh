136f9c209c505be2017848cf54b87d61f5f5887a
d.shabalin <d.shabalin@ticketland.ru>
2021-01-04 01:31:51 +0300

    Спуск в безенги

--- photos/resize.sh
+++ photos/resize.sh
@@ -1,10 +1,10 @@
 for dir in ./*; do
   echo ${dir}
   mkdir -p  "preview/${dir}"
-#  for i in ./${dir}/*.JPG; do
-#      convert "$i" -resize 20% -quality 70 "preview/${dir}/${i##*/}"
-#  done
-  for i in ./${dir}/*.jpg; do
+  for i in ./${dir}/*.JPG; do
       convert "$i" -resize 20% -quality 70 "preview/${dir}/${i##*/}"
   done
+#  for i in ./${dir}/*.jpg; do
+#      convert "$i" -resize 20% -quality 70 "preview/${dir}/${i##*/}"
+#  done
 done
